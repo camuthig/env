ee3be86dd8279cc7fe058ac6b168f23b2bf98269
Chris Muthig <camuthig@gmail.com>
2018-03-21 22:30:33 -0700

    Move zsh configuration to the end as it will stop execution

--- setup.sh
+++ setup.sh
@@ -30,13 +30,12 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 echo Copying vimrc
 cp vimrc ~/.vimrc
 
+# Set gitconfig
+echo Setting up gitconfig
+cp gitconfig ~/.gitconfig
+
 # Install oh-my-zsh
 echo Installing oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 cp zshrc ~/.zshrc
-chsh -s $(which zsh)
-
-# Set gitconfig
-echo Setting up gitconfig
-cp gitconfig ~/.gitconfig
 
