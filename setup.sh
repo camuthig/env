c4b3eb1f01277eb79faaf735a91eecc44e77f5d0
Chris Muthig <camuthig@gmail.com>
2018-04-06 21:45:06 -0700

    Copy zsh config before installing

--- setup.sh
+++ setup.sh
@@ -36,6 +36,6 @@ cp gitconfig ~/.gitconfig
 
 # Install oh-my-zsh
 echo Installing oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 cp zshrc ~/.zshrc
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
